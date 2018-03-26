197e7ca6ac8709d26b2d91f4aaf67e087c9a7ba9
Matías Pavez <matias.pavez.b@gmail.com>
2018-03-26 17:47:20 -0300

    flags: test suite

---
+++ duolingo_background_flags_tests.js
@@ -0,0 +1,45 @@
+/*
+=======================
+Test Suite
+=======================
+
+URLS:
+================
+- Discussion:
+	- home        : https://www.duolingo.com/discussion
+	- short thread: https://www.duolingo.com/comment/26685160
+    - long  thread: 
+- Home Page  : https://www.duolingo.com/
+- Labs       : https://www.duolingo.com/labs
+- Store      : https://www.duolingo.com/show_store
+- User       : https://www.duolingo.com/mpavezb
+- Words      : https://www.duolingo.com/words
+- Skill info : https://www.duolingo.com/skill/ja/Hiragana-2
+
+
+Usability:
+================
+- Page changes on Language selection
+- Adapts to different screen sizes:
+	- Full screen
+	- Middle size
+- Browser support: Firefox, Chrome
+
+
+Performance
+================
+- by #languages
+	- ~  5 languages:
+		- https://www.duolingo.com/mpavezb
+	    - https://www.duolingo.com/users/mpavezb 
+
+- by # followers
+	- ~1000 followers:
+		- https://www.duolingo.com/FieryCat
+		- https://www.duolingo.com/users/FieryCat
+	- ~  10 followers:
+		- https://www.duolingo.com/mpavezb
+	    - https://www.duolingo.com/users/mpavezb
+
+*/
+
